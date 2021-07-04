e11fde38826ea8577bffef7f13556a331fcc2d2b
Cesar Poumian <cepoumian@Cesars-MacBook-Pro.local>
2021-07-03 22:49:09 -0600

    Fist Commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react'
+
+import HeaderCartButton from './HeaderCartButton'
+import mealsImage from '../../assets/meals.jpeg'
+import classes from './Header.module.css'
+
+export default function Header() {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>React Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table with food' />
+      </div>
+    </Fragment>
+  )
+}
